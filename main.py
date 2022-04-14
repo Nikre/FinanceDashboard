94b5ff347681e1a5e270917f1a6a1e7617cc8825
Nicolo <monarca.nicolo@gmail.com>
2022-04-14 22:44:53 +0200

    [ADD] - Aggiunta la lista delle azioni

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+stocks_list = ['A2A.MI', 'AMP.MI', 'ATL.MI', 'ATL.MI', 'BGN.MI',
+    'BMED.MI', 'BAMI.MI', 'BPE.MI', 'CPR.MI', 'CNHI.MI', 
+    'DIA.MI', 'ENEL.MI', 'ENI.MI', 'EXO.MI', 'RACE.MI', 
+    'FBK.MI', 'G.MI', 'HER.MI', 'IP.MI', 'ISP.MI', 
+    'INW.MI', 'IG.MI', 'IVG.MI', 'LDO.MI', 'MB.MI', 
+    'MONC.MI', 'NEXI.MI', 'PIRC.MI', 'PST.MI', 'PRY.MI', 
+    'REC.MI', 'SPM.MI', 'SRG.MI', 'STLA.MI', 'STM.MI', 
+    'TIT.MI', 'TEN.MI', 'TRN.MI', 'UCG.MI', 'UNI.MI']
+
 if __name__ == '__main__':
     print('Ciao')
