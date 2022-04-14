63110b91ef018f94155e0587c4bd719fff5258e3
Nicolo <monarca.nicolo@gmail.com>
2022-04-14 22:55:04 +0200

    [ADD] - Aggiunto il multhreading

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import yfinance as yf
+import threading
+
+
+def analysis(stock):
+    print(stock)
+
 stocks_list = ['A2A.MI', 'AMP.MI', 'ATL.MI', 'ATL.MI', 'BGN.MI',
     'BMED.MI', 'BAMI.MI', 'BPE.MI', 'CPR.MI', 'CNHI.MI', 
     'DIA.MI', 'ENEL.MI', 'ENI.MI', 'EXO.MI', 'RACE.MI', 
@@ -7,5 +14,16 @@ stocks_list = ['A2A.MI', 'AMP.MI', 'ATL.MI', 'ATL.MI', 'BGN.MI',
     'REC.MI', 'SPM.MI', 'SRG.MI', 'STLA.MI', 'STM.MI', 
     'TIT.MI', 'TEN.MI', 'TRN.MI', 'UCG.MI', 'UNI.MI']
 
+thread_list = []
+
 if __name__ == '__main__':
     print('Ciao')
+    for stock in stocks_list:
+        t = threading.Thread(target=analysis, args=(stock, ))
+        thread_list.append(t)
+
+    for thread in thread_list:
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
