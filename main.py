165f25b665981d46771396337ed675d415756c6e
Nicolo <monarca.nicolo@gmail.com>
2022-04-16 00:07:09 +0200

    [ADD] - Aggiunto il check giornaliero

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+from operator import length_hint
+from numpy import append
 import yfinance as yf
 import pandas as pd
+import pandas_ta as ta
 import time
+import json
 
 def analysis(stock):
     print(stock)
     data = yf.download(tickers=stock, period='6mo', interval='1d')
-    print(data.tail(3))
+    data['SMA20'] = data['Close'].rolling(20).mean()
+    data['SMA200'] = data['Close'].rolling(200).mean()
+    data['BBU20'] = data['SMA20'] + 2.5 * data['Close'].rolling(20).std()
+    data['BBL20'] = data['SMA20'] - 2.5 * data['Close'].rolling(20).std()
+    data.ta.rsi(close='Close', length=14, append=True)
+    print(data.tail())
+    check_entry_conditions(stock, data.tail(1))
+
+def check_entry_conditions(stock, data):
+    if (data['Close'].values[0] <= data['BBL20'].values[0]):
+        entry = {'Titolo': stock, 
+                'Chiusura': data['Close'], 
+                'BBL20': data['BBL20'], 
+                'Prezzo entrata': data['Close'] * 0.97}
+        possible_entry['operations'].append(entry)
 
 stocks_list = ['A2A.MI', 'AMP.MI', 'ATL.MI', 'ATL.MI', 'BGN.MI',
     'BMED.MI', 'BAMI.MI', 'BPE.MI', 'CPR.MI', 'CNHI.MI', 
@@ -16,10 +34,12 @@ stocks_list = ['A2A.MI', 'AMP.MI', 'ATL.MI', 'ATL.MI', 'BGN.MI',
     'REC.MI', 'SPM.MI', 'SRG.MI', 'STLA.MI', 'STM.MI', 
     'TIT.MI', 'TEN.MI', 'TRN.MI', 'UCG.MI', 'UNI.MI']
 
-thread_list = []
+possible_entry = {'operations': []}
 
 if __name__ == '__main__':
     print('Ciao')
     for stock in stocks_list:
         analysis(stock=stock)
-       
+    
+    with open('data.json', 'w') as fp:
+        json.dump(possible_entry, fp)
