b0fea314f86125360ed481987d42aba4cebc75fe
Nicolo <monarca.nicolo@gmail.com>
2022-04-16 09:27:43 +0200

    [ADD] - Predisposizione per il backtesting

--- main.py
+++ main.py
@@ -7,15 +7,24 @@ import time
 import json
 
 def analysis(stock):
-    print(stock)
     data = yf.download(tickers=stock, period='6mo', interval='1d')
+    data = add_indicators(data)
+    check_entry_conditions(stock, data.tail(1))
+
+def back_testing(stock):
+    data = yf.download(tickers=stock, period='5y', interval='1d')
+    data = add_indicators(data)
+    for index, row in data.iterrows():
+        print(index)
+        time.sleep(0.1)
+
+def add_indicators(data):
     data['SMA20'] = data['Close'].rolling(20).mean()
     data['SMA200'] = data['Close'].rolling(200).mean()
     data['BBU20'] = data['SMA20'] + 2.5 * data['Close'].rolling(20).std()
     data['BBL20'] = data['SMA20'] - 2.5 * data['Close'].rolling(20).std()
     data.ta.rsi(close='Close', length=14, append=True)
-    print(data.tail())
-    check_entry_conditions(stock, data.tail(1))
+    return data
 
 def check_entry_conditions(stock, data):
     if (data['Close'].values[0] <= data['BBL20'].values[0]):
@@ -35,11 +44,13 @@ stocks_list = ['A2A.MI', 'AMP.MI', 'ATL.MI', 'ATL.MI', 'BGN.MI',
     'TIT.MI', 'TEN.MI', 'TRN.MI', 'UCG.MI', 'UNI.MI']
 
 possible_entry = {'operations': []}
+back_testing_opearions = {'operations': []}
 
 if __name__ == '__main__':
     print('Ciao')
     for stock in stocks_list:
-        analysis(stock=stock)
-    
+        # analysis(stock=stock)
+        back_testing(stock=stock)
+        
     with open('data.json', 'w') as fp:
         json.dump(possible_entry, fp)
